7ad57f9f66190c0d56fa1bfb548c71687a0cb5b8
kashifa1995 <112203711+kashifa1995@users.noreply.github.com>
2022-10-28 03:38:58 +0530

    adding amazon scenario

--- google.feature
+++ google.feature
@@ -8,3 +8,10 @@ Feature: Login
         When I enter valid username and password
         And I click on Submit button
         Then login is successful
+
+    Scenario: Amazon login Test
+        Given I navigate to amazon.in
+        When I click on sign up
+        And I enter username and password for amazon
+        And I click on signIn button
+        Then Kashi is seen on login
--- steps/steps.py
+++ steps/steps.py
@@ -19,3 +19,24 @@ def step_impl(context):
 @then(u'login is successful')
 def step_impl(context):
     pass
+
+@given(u'I navigate to amazon.in')
+def step_impl(context):
+    context.browser.get('https://www.amazon.in/')
+
+@when(u'I click on sign up')
+def step_impl(context):
+    context.browser.find_element_by_xpath("//span[.='Hello, sign in']").click()
+
+@when(u'I enter username and password for amazon')
+def step_impl(context):
+    context.browser.find_element_by_id("ap_email").send_keys("kashifamansoor1995@gmail.com")
+    context.browser.find_element_by_id("continue").click()
+    context.browser.find_element_by_id("ap_password").send_keys("chulbul2020")
+@when(u'I click on signIn button')
+def step_impl(context):
+    context.browser.find_element_by_id("signInSubmit").click()
+
+@then(u'Kashi is seen on login')
+def step_impl(context):
+    assert context.browser.find_element_by_xpath("//span[.='Hello, Kashi']").is_displayed()==True
